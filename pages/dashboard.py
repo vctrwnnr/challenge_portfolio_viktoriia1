ea84787c8f016ae8dcc6b9649844bb2ebafdfec9
Viktoriia <vctr.wnnr@gmail.com>
2022-09-06 14:19:16 +0300

    1

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -23,6 +23,4 @@ class Dashboard(BasePage):
     def click_on_the_add_player_button(self):
         self.click_on_the_element(self.add_player_button_xpath)
 
-
-
     pass
---
+++ test_cases/all_suite_test.py
@@ -0,0 +1,19 @@
+import unittest
+
+from unittest.loader import makeSuite
+
+
+from test_cases.framework import Test
+from test_cases.login_to_the_system import TestLoginPage
+
+
+def full_suite():
+   test_suite = unittest.TestSuite()
+   test_suite.addTest(makeSuite(TestLoginPage))
+   test_suite.addTest(makeSuite(Test))
+   return test_suite
+
+
+if __name__ == '__main__':
+   runner = unittest.TextTestRunner(verbosity=2)
+   runner.run(full_suite())
